633771801d3617227b3dd21c03f829746b12db75
Gabriel Nardes <gabrielnardeslobao@gmail.com>
2021-02-18 23:00:52 -0300

    Add empty string check for saveCover()

--- routes/books.js
+++ routes/books.js
@@ -136,8 +136,10 @@ async function renderFormPage(res, book, form, hasError = false) {
 }
 
 function saveCover(book, coverEncoded) {
-    if (coverEncoded == null) return;
+    if (coverEncoded == null || coverEncoded == '') return;
+    console.log("befpre");
     const cover = JSON.parse(coverEncoded);
+    console.log("after");
     if (cover != null && imageMimeTypes.includes(cover.type)) {
         book.coverImage = new Buffer.from(cover.data, 'base64');
         book.coverImageType = cover.type;
