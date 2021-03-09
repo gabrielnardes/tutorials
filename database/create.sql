d042630129114bb74ad92adeeb6a4e92251c1a03
Gabriel Nardes <gabrielnardeslobao@gmail.com>
2021-03-09 09:23:59 -0300

    Add files

--- database/create.sql
+++ database/create.sql
@@ -6,3 +6,23 @@ create table blog.post (
     content text not null,
     date timestamp default now()
 ); 
+
+/*
+
+create schema blog;
+
+create table blog.post (
+    id serial primary key,
+    title text not null,
+    content text not null,
+    date timestamp default now()
+);
+
+
+insert into blog.post(title, content) values ('REST API: Métodos', '...');
+insert into blog.post(title, content) values ('REST API: Introdução', '...');
+insert into blog.post(title, content) values ('REST API: Documentação', '...');
+
+select * from blog.post
+
+*/
