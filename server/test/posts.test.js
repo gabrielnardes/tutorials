57c03b2dd1654abe34c49fddb3b39afb3b765b5e
Gabriel Nardes <gabrielnardeslobao@gmail.com>
2021-03-11 23:24:02 -0300

    Add error codes

--- server/test/posts.test.js
+++ server/test/posts.test.js
@@ -7,7 +7,7 @@ const generate = function () {
 };
 
 const request = function (url, method, data) {
-    return axios({ url, method, data });
+    return axios({ url, method, data, validateStatus: false });
 };
 
 test('Should get posts', async function () {
@@ -26,6 +26,8 @@ test('Should get posts', async function () {
 
     const response = await request('http://localhost:3000/posts', 'get');
 
+    expect(response.status).toBe(200);
+
     const posts = response.data;
     expect(posts).toHaveLength(3);
 
@@ -34,17 +36,42 @@ test('Should get posts', async function () {
     await postsService.deletePost(post3.id);
 });
 
-test('Should save post', async function () {
+test('Should save a post', async function () {
     const data = {
         title: generate(),
         content: generate(),
     };
 
     const response = await request('http://localhost:3000/posts', 'post', data);
+    expect(response.status).toBe(201);
+
     const post = response.data;
 
     expect(post.title).toBe(data.title);
     expect(post.content).toBe(data.content);
+
+    await postsService.deletePost(post.id);
+});
+
+test('Should not a save post', async function () {
+    const data = {
+        title: generate(),
+        content: generate(),
+    };
+
+    const response1 = await request(
+        'http://localhost:3000/posts',
+        'post',
+        data
+    );
+    const response2 = await request(
+        'http://localhost:3000/posts',
+        'post',
+        data
+    );
+    expect(response2.status).toBe(409);
+
+    const post = response1.data;
 
     await postsService.deletePost(post.id);
 });
@@ -58,7 +85,12 @@ test('Should update post', async function () {
     post.title = generate();
     post.content = generate();
 
-    await request(`http://localhost:3000/posts/${post.id}`, 'put', post);
+    const response = await request(
+        `http://localhost:3000/posts/${post.id}`,
+        'put',
+        post
+    );
+    expect(response.status).toBe(204);
 
     const updatedPost = await postsService.getPost(post.id);
 
@@ -68,13 +100,29 @@ test('Should update post', async function () {
     await postsService.deletePost(post.id);
 });
 
+test('Should not update post', async function () {
+    const post = { id: 1 };
+
+    const response = await request(
+        `http://localhost:3000/posts/${post.id}`,
+        'put',
+        post
+    );
+    expect(response.status).toBe(404);
+});
+
 test('Should delete a post', async function () {
     const post = await postsService.savePost({
         title: generate(),
         content: generate(),
     });
 
-    await request(`http://localhost:3000/posts/${post.id}`, 'delete', post);
+    const response = await request(
+        `http://localhost:3000/posts/${post.id}`,
+        'delete',
+        post
+    );
+    expect(response.status).toBe(204);
 
     const posts = await postsService.getPosts();
 
